6cfda8d36429c15c068999ad676d3ee52199e98c
‭ <>
2025-11-05 05:35:12 +0800

    1.9.9.1

--- common/global_ui.js
+++ common/global_ui.js
@@ -10,12 +10,20 @@ HTMLElement.prototype.hide = function() {
 
 // Disables the browser's internal password manager and let the extension take the control
 const updateDefaultPasswordManager = async function() {
-    const passwordSavingEnabled = await browser.privacy.services.passwordSavingEnabled.get({});
-    if ((passwordSavingEnabled?.levelOfControl === 'controlled_by_this_extension'
-        || passwordSavingEnabled?.levelOfControl === 'controllable_by_this_extension')
-    ) {
-        await browser.privacy.services.passwordSavingEnabled.set({
-            value: !passwordSavingEnabled.value,
-        });
+    const permissionResponse = await browser.permissions.request({ permissions: [ 'privacy' ] });
+    if (!permissionResponse) {
+        return false;
     }
+
+    const passwordSavingEnabled = await browser.privacy.services.passwordSavingEnabled.get({});
+    if (passwordSavingEnabled?.levelOfControl === 'controllable_by_this_extension') {
+        await browser.privacy.services.passwordSavingEnabled.set({
+            value: false,
+        });
+        return true;
+    } else if (passwordSavingEnabled?.levelOfControl === 'controlled_by_this_extension') {
+        await browser.privacy.services.passwordSavingEnabled.clear({});
+    }
+
+    return false;
 };
